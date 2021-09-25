3e2dc1a8a266af7067c5cb2566ad9df21814d974
salihsan <salih.kildis@gmail.com>
2021-09-25 12:29:47 +0300

    rakamlari toplamini veren program

---
+++ JAVA101_Odevler/src/main/java/odevler/donguler/BasamakDegerlerininToplami.java
@@ -0,0 +1,25 @@
+package odevler.donguler;
+
+import java.util.Scanner;
+
+public class BasamakDegerlerininToplami {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayi giriniz : ");
+        int s = scan.nextInt();
+        //136
+        int adet=0;
+        int rakamlarToplami=0;
+
+            while (s != 0){
+                rakamlarToplami+= s%10;
+                s/=10;
+                adet++;
+            }
+        System.out.println(adet+" Basamakli Sayinin Rakamlar Toplami : "+rakamlarToplami);
+
+
+    }
+
+}
+
