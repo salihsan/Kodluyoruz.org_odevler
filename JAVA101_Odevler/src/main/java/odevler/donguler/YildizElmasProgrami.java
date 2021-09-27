1db49fa5740c9239d5acb5de1ae03bce43390027
salihsan <salih.kildis@gmail.com>
2021-09-27 07:00:47 +0300

    for loop elmas yorumdan cikar

--- JAVA101_Odevler/src/main/java/odevler/donguler/YildizElmasProgrami.java
+++ JAVA101_Odevler/src/main/java/odevler/donguler/YildizElmasProgrami.java
@@ -26,20 +26,20 @@ public class YildizElmasProgrami {
         }
 
 
-//        space = 1;
-//        for (j = 1; j<= n - 1; j++)
-//        {
-//            for (i = 1; i<= space; i++)
-//            {
-//                System.out.print(" ");
-//            }
-//            space++;
-//            for (i = 1; i<= 2 * (n - j) - 1; i++)
-//            {
-//                System.out.print("*");
-//            }
-//            System.out.println("");
-//        }
+        space = 1;
+        for (j = 1; j<= n - 1; j++)
+        {
+            for (i = 1; i<= space; i++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for (i = 1; i<= 2 * (n - j) - 1; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
 
