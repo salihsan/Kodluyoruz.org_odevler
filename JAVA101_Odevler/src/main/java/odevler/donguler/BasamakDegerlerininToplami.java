2d13d7b4ac8cc91f7e15191e750350337a867538
salihsan <salih.kildis@gmail.com>
2021-10-01 18:59:12 +0300

    swithcase Atm Programi

--- JAVA101_Odevler/src/main/java/odevler/donguler/BasamakDegerlerininToplami.java
+++ JAVA101_Odevler/src/main/java/odevler/donguler/BasamakDegerlerininToplami.java
@@ -19,7 +19,7 @@ public class BasamakDegerlerininToplami {
                 adet++;
             }
         System.out.println(adet+" Basamakli Sayinin Rakamlar Toplami : "+rakamlarToplami);
-
+    // Rakamlari toplami
     }
 
 }
