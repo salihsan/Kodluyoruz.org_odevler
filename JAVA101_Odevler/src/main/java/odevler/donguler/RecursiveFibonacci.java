d2e2e2223c0aec9dc59f55be81bc286217595bd0
salihsan <salih.kildis@gmail.com>
2021-10-13 15:35:56 +0300

    Recursive metot ile Fibonacci serisi Programi

---
+++ JAVA101_Odevler/src/main/java/odevler/donguler/RecursiveFibonacci.java
@@ -0,0 +1,12 @@
+package odevler.donguler;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+    static int fibo(int n){
+        if (n==1 || n==2){
+            return 1;        }
+        return  fibo(n-1)+fibo(n-2);
+    }
+}
